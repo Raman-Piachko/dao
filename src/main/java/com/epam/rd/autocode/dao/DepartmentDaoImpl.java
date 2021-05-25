37b257baad7d91548489ffc904269caf74a4dccf
Raman-Piachko <rpechko88@gmail.com>
2021-05-25 11:23:37 +0300

    after fixing

--- src/main/java/com/epam/rd/autocode/dao/DepartmentDaoImpl.java
+++ src/main/java/com/epam/rd/autocode/dao/DepartmentDaoImpl.java
@@ -2,9 +2,11 @@ package com.epam.rd.autocode.dao;
 
 import com.epam.rd.autocode.ConnectionSource;
 import com.epam.rd.autocode.domain.Department;
+import com.epam.rd.autocode.exception.DaoException;
 
 import java.math.BigInteger;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,36 +15,47 @@ import java.util.List;
 import java.util.Optional;
 
 public class DepartmentDaoImpl implements DepartmentDao {
-    private static final String SELECT_DEPARTMENT_BY_ID = "select * from DEPARTMENT where ID = %s";
-    private static final String SELECT_ALL = "select * from DEPARTMENT";
-    private static final String DELETE = "delete from DEPARTMENT where ID = '%s'";
-    private static final String UPDATE = "update DEPARTMENT set NAME = '%s', LOCATION = '%s' where ID = '%s'";
-    private static final String INSERT = "insert into DEPARTMENT (ID, NAME, LOCATION) values ('%s', '%s', '%s')";
-    private static final String ID = "ID";
-    private static final String NAME = "NAME";
-    private static final String LOCATION = "LOCATION";
+    private static final String SQL_QUERY_SELECT_BY_ID = "SELECT * FROM department WHERE id = ?";
+    private static final String SQL_QUERY_DELETE = "DELETE FROM department WHERE id = ?";
+    private static final String SQL_QUERY_SELECT_ALL = "SELECT * FROM department";
+    private static final String SQL_QUERY_UPDATE = "UPDATE department SET name = '%s', location = '%s' WHERE id = '%s'";
+    private static final String SQL_QUERY_INSERT = "INSERT INTO department (id, name, location) VALUES ('%s', '%s', '%s')";
+
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_LOCATION = "location";
 
     @Override
-    public Optional<Department> getById(BigInteger Id) {
-            return Optional.ofNullable(getDepartment(String.format(SELECT_DEPARTMENT_BY_ID, Id)));
+    public Optional<Department> getById(BigInteger id) {
+        Optional<Department> department = Optional.empty();
+        try (Connection connection = ConnectionSource.instance().createConnection();
+             PreparedStatement statement = connection.prepareStatement(SQL_QUERY_SELECT_BY_ID)) {
+            statement.setLong(1, id.longValue());
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    department = Optional.of(createDepartment(resultSet));
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new DaoException("Something went wrong at getById", e);
+        }
+        return department;
     }
+
 
     @Override
     public List<Department> getAll() {
         List<Department> departments = new ArrayList<>();
-        try {
-            Connection connection = ConnectionSource.instance().createConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(SELECT_ALL);
+        try (Connection connection = ConnectionSource.instance().createConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(SQL_QUERY_SELECT_ALL)) {
             while (resultSet.next()) {
-                Department department = new Department(
-                        new BigInteger(resultSet.getString(ID)),
-                        resultSet.getString(NAME),
-                        resultSet.getString(LOCATION));
+                Department department = createDepartment(resultSet);
                 departments.add(department);
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DaoException("Something went wrong at getAll", e);
         }
         return departments;
     }
@@ -52,52 +65,48 @@ public class DepartmentDaoImpl implements DepartmentDao {
         try {
             Connection connection = ConnectionSource.instance().createConnection();
             Statement statement = connection.createStatement();
-            if (getById(department.getId()).isPresent()) {
+            Optional<Department> foundDepartment = getById(department.getId());
+            if (foundDepartment.isPresent()) {
                 statement.executeUpdate(String.format(
-                        UPDATE,
+                        SQL_QUERY_UPDATE,
                         department.getName(),
                         department.getLocation(),
                         department.getId().toString()));
             } else {
                 statement.executeUpdate(String.format(
-                        INSERT,
+                        SQL_QUERY_INSERT,
                         department.getId().toString(),
                         department.getName(),
                         department.getLocation())
                 );
             }
+            return department;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DaoException("Something went wrong at save", e);
         }
-        return department;
     }
 
     @Override
     public void delete(Department department) {
-        try {
-            Connection connection = ConnectionSource.instance().createConnection();
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(String.format(DELETE, department.getId().toString()));
+        try (Connection connection = ConnectionSource.instance().createConnection();
+             PreparedStatement statement = connection.prepareStatement(SQL_QUERY_DELETE)) {
+            statement.setLong(1, department.getId().longValue());
+            statement.executeUpdate();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DaoException("Something went wrong at delete", e);
         }
     }
 
-    private Department getDepartment(String query) {
-        Department department = null;
+    private Department createDepartment(ResultSet resultSet) {
+        Department department;
         try {
-            Connection connection = ConnectionSource.instance().createConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-            while (resultSet.next()) {
-                department = new Department(
-                        new BigInteger(resultSet.getString(ID)),
-                        resultSet.getString(NAME),
-                        resultSet.getString(LOCATION)
-                );
-            }
+            BigInteger id = BigInteger.valueOf(resultSet.getInt(COLUMN_ID));
+            String name = resultSet.getString(COLUMN_NAME);
+            String location = resultSet.getString(COLUMN_LOCATION);
+            department = new Department(id, name, location);
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DaoException("Something went wrong at createDepartment", e);
         }
         return department;
     }
--- src/main/java/com/epam/rd/autocode/dao/EmployeeDaoImpl.java
+++ src/main/java/com/epam/rd/autocode/dao/EmployeeDaoImpl.java
@@ -5,85 +5,93 @@ import com.epam.rd.autocode.domain.Department;
 import com.epam.rd.autocode.domain.Employee;
 import com.epam.rd.autocode.domain.FullName;
 import com.epam.rd.autocode.domain.Position;
+import com.epam.rd.autocode.exception.DaoException;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class EmployeeDaoImpl implements EmployeeDao {
-    private static final String SELECT_FROM_EMPLOYEE_BY_ID = "select * from EMPLOYEE where ID = %s";
-    private static final String SELECT_ALL = "select * from EMPLOYEE";
-    private static final String DELETE = "delete from EMPLOYEE where ID = '%s'";
-    private static final String ID = "ID";
-    private static final String FIRSTNAME = "FIRSTNAME";
-    private static final String LASTNAME = "LASTNAME";
-    private static final String MIDDLENAME = "MIDDLENAME";
-    private static final String POSITION = "POSITION";
-    private static final String HIREDATE = "HIREDATE";
-    private static final String SALARY = "SALARY";
-    private static final String MANAGER = "MANAGER";
-    private static final String DEPARTMENT = "DEPARTMENT";
-    private static final String UPDATE = "update EMPLOYEE set " +
-            "FIRSTNAME = '%s', " +
-            "LASTNAME = '%s', " +
-            "MIDDLENAME = '%s', " +
-            "POSITION = '%s', " +
-            "HIREDATE = '%s', " +
-            "SALARY = '%s', " +
-            "MANAGER = '%s', " +
-            "DEPARTMENT = '%s', " +
-            "' where ID = '%s'";
-    private static final String INSERT = "insert into EMPLOYEE (ID, FIRSTNAME, LASTNAME, MIDDLENAME, POSITION, HIREDATE," +
-            " SALARY, MANAGER, DEPARTMENT) values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')";
+    private static final String SQL_QUERY_SELECT_BY_ID = "SELECT * FROM employee WHERE id = ?";
+    private static final String SQL_QUERY_SELECT_BY_MANAGER = "SELECT * FROM employee WHERE manager = ?";
+    private static final String SQL_QUERY_SELECT_BY_DEPARTMENT =
+            "SELECT * FROM employee WHERE department = ?";
+    private static final String SQL_QUERY_DELETE = "DELETE FROM employee WHERE id = %s";
+    private static final String SQL_QUERY_INSERT = "INSERT INTO employee (id, firstname, lastname, middlename," +
+            " position, hiredate, salary, manager, department) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')";
+    private static final String SQL_QUERY_SELECT_ALL = "SELECT * FROM employee";
+    private static final String SQL_QUERY_UPDATE = "UPDATE employee SET " +
+            "firstname = '%s', " +
+            "lastname = '%s', " +
+            "middlename = '%s', " +
+            "position = '%s', " +
+            "hiredate = '%s', " +
+            "salary = '%s', " +
+            "manager = '%s', " +
+            "department = '%s', " +
+            "' WHERE id = '%s'";
+
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_SALARY = "salary";
+    private static final String COLUMN_HIREDATE = "hiredate";
+    private static final String COLUMN_FIRSTNAME = "firstname";
+    private static final String COLUMN_LASTNAME = "lastname";
+    private static final String COLUMN_MIDDLENAME = "middlename";
+    private static final String COLUMN_POSITION = "position";
+    private static final String COLUMN_MANAGER = "manager";
+    private static final String COLUMN_DEPARTMENT = "department";
 
     @Override
-    public Optional<Employee> getById(BigInteger Id) {
-        return Optional.ofNullable(getEmployee(String.format(SELECT_FROM_EMPLOYEE_BY_ID, Id)));
+    public Optional<Employee> getById(BigInteger id) {
+        Optional<Employee> employee = Optional.empty();
+        try (Connection connection = ConnectionSource.instance().createConnection();
+             PreparedStatement statement = connection.prepareStatement(SQL_QUERY_SELECT_BY_ID)) {
+            statement.setLong(1, id.longValue());
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    employee = Optional.of(createEmployee(resultSet));
+                }
+            }
+        } catch (SQLException e) {
+            throw new DaoException("Something went wrong at getById", e);
+        }
+
+        return employee;
     }
+
 
     @Override
     public List<Employee> getAll() {
         List<Employee> employees = new ArrayList<>();
-        try {
-            Connection connection = ConnectionSource.instance().createConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(SELECT_ALL);
-
+        try (Connection connection = ConnectionSource.instance().createConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(SQL_QUERY_SELECT_ALL)) {
             while (resultSet.next()) {
-                BigInteger id = new BigInteger(resultSet.getString(ID));
-                FullName fullName = new FullName(
-                        resultSet.getString(FIRSTNAME),
-                        resultSet.getString(LASTNAME),
-                        resultSet.getString(MIDDLENAME)
-                );
-                Position position = Position.valueOf(resultSet.getString(POSITION));
-                LocalDate hiredate = resultSet.getDate(HIREDATE).toLocalDate();
-                BigDecimal salary = resultSet.getBigDecimal(SALARY);
-                BigInteger manager = new BigInteger(String.valueOf(resultSet.getInt(MANAGER)));
-                BigInteger department = new BigInteger(String.valueOf(resultSet.getInt(DEPARTMENT)));
-
-                Employee employee = new Employee(id, fullName, position, hiredate, salary, manager, department);
+                Employee employee = createEmployee(resultSet);
                 employees.add(employee);
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DaoException("Something went wrong at getAll", e);
         }
+
         return employees;
     }
 
     @Override
     public Employee save(Employee employee) {
-        String firstName = employee.getFullName().getFirstName();
-        String lastName = employee.getFullName().getLastName();
-        String middleName = employee.getFullName().getMiddleName();
+        FullName fullName = employee.getFullName();
+        String firstName = fullName.getFirstName();
+        String lastName = fullName.getLastName();
+        String middleName = fullName.getMiddleName();
         Position position = employee.getPosition();
         LocalDate hiredate = employee.getHired();
         BigDecimal salary = employee.getSalary();
@@ -91,83 +99,111 @@ public class EmployeeDaoImpl implements EmployeeDao {
         BigInteger departmentId = employee.getDepartmentId();
         BigInteger id = employee.getId();
 
-        try {
-            Connection connection = ConnectionSource.instance().createConnection();
-            Statement statement = connection.createStatement();
-            if (getById(employee.getId()).isPresent()) {
-                statement.executeUpdate(String.format(UPDATE, firstName, lastName, middleName, position, hiredate,
-                        salary, managerId, departmentId, id));
+        try (Connection connection = ConnectionSource.instance().createConnection();
+             Statement statement = connection.createStatement()) {
+            Optional<Employee> foundEmployee = getById(employee.getId());
+            if (foundEmployee.isPresent()) {
+                statement.executeUpdate(String.format(SQL_QUERY_UPDATE,
+                        firstName, lastName, middleName,
+                        position, hiredate, salary,
+                        managerId, departmentId, id));
             } else {
-                statement.executeUpdate(String.format(INSERT, id, firstName, lastName, middleName, position, hiredate,
+                statement.executeUpdate(String.format(SQL_QUERY_INSERT,
+                        id, firstName, lastName,
+                        middleName, position, hiredate,
                         salary, managerId, departmentId, id));
             }
+
+            return employee;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DaoException("Something went wrong at save", e);
         }
-        return getEmployee(String.format(SELECT_FROM_EMPLOYEE_BY_ID, employee.getId()));
     }
 
     @Override
     public void delete(Employee employee) {
-        try {
-            Connection connection = ConnectionSource.instance().createConnection();
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(String.format(DELETE, employee.getId().toString()));
+        try (Connection connection = ConnectionSource.instance().createConnection();
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(String.format(SQL_QUERY_DELETE, employee.getId().toString()));
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DaoException("Something went wrong at delete", e);
         }
     }
 
     @Override
     public List<Employee> getByDepartment(Department department) {
-        return getAll().stream()
-                .filter(employee -> employee.getDepartmentId().equals(department.getId()))
-                .collect(Collectors.toList());
+        List<Employee> employees = new ArrayList<>();
+        try (Connection connection = ConnectionSource.instance().createConnection();
+             PreparedStatement statement = connection.prepareStatement(SQL_QUERY_SELECT_BY_DEPARTMENT)) {
+            statement.setLong(1, department.getId().longValue());
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    Employee employee = createEmployee(resultSet);
+                    employees.add(employee);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new DaoException("Something went wrong at getByDepartment", e);
+        }
+
+        return employees;
     }
 
     @Override
-    public List<Employee> getByManager(Employee employee) {
-        return getAll().stream()
-                .filter(e -> e.getManagerId().equals(employee.getId()))
-                .collect(Collectors.toList());
-    }
-
-    private Employee getEmployee(String query) {
-        Employee employee = null;
-        try {
-            Connection connection = ConnectionSource.instance().createConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-
-            while (resultSet.next()) {
-                Position position = getPosition(resultSet);
-                FullName fullName = getFullName(resultSet);
-                BigInteger id = new BigInteger(resultSet.getString(ID));
-                LocalDate hired = getHiredate(resultSet);
-                BigDecimal salary = resultSet.getBigDecimal(SALARY);
-                BigInteger managerId = BigInteger.valueOf(resultSet.getInt(MANAGER));
-                BigInteger depatmentId = BigInteger.valueOf(resultSet.getInt(DEPARTMENT));
-
-                employee = new Employee(id, fullName, position, hired, salary, managerId, depatmentId);
+    public List<Employee> getByManager(Employee manager) {
+        List<Employee> employees = new ArrayList<>();
+        try (Connection connection = ConnectionSource.instance().createConnection();
+             PreparedStatement statement = connection.prepareStatement(SQL_QUERY_SELECT_BY_MANAGER)) {
+            statement.setLong(1, manager.getId().longValue());
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    Employee employee = createEmployee(resultSet);
+                    employees.add(employee);
+                }
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DaoException("Something went wrong at getByManager", e);
         }
+
+        return employees;
+    }
+
+    private Employee createEmployee(ResultSet resultSet) {
+        Employee employee;
+        try {
+            Position position = getPosition(resultSet);
+            FullName fullName = getFullName(resultSet);
+            BigInteger id = BigInteger.valueOf(resultSet.getInt(COLUMN_ID));
+            LocalDate hired = getHired(resultSet);
+            BigDecimal salary = resultSet.getBigDecimal(COLUMN_SALARY);
+            BigInteger managerId = new BigInteger(String.valueOf(resultSet.getInt(COLUMN_MANAGER)));
+            BigInteger depatmentId = new BigInteger(String.valueOf(resultSet.getInt(COLUMN_DEPARTMENT)));
+            employee = new Employee(id, fullName, position, hired, salary, managerId, depatmentId);
+        } catch (SQLException e) {
+            throw new DaoException("Something went wrong at createEmployee", e);
+        }
+
         return employee;
     }
 
-    private LocalDate getHiredate(ResultSet resultSet) throws SQLException {
-        return resultSet.getDate(HIREDATE).toLocalDate();
+    private LocalDate getHired(ResultSet resultSet) throws SQLException {
+        Date rawDate = resultSet.getDate(COLUMN_HIREDATE);
+
+        return LocalDate.parse(rawDate.toString());
     }
 
     private FullName getFullName(ResultSet resultSet) throws SQLException {
-        String firstName = resultSet.getString(FIRSTNAME);
-        String lastName = resultSet.getString(LASTNAME);
-        String middleName = resultSet.getString(MIDDLENAME);
+        String firstName = resultSet.getString(COLUMN_FIRSTNAME);
+        String lastName = resultSet.getString(COLUMN_LASTNAME);
+        String middleName = resultSet.getString(COLUMN_MIDDLENAME);
+
         return new FullName(firstName, lastName, middleName);
     }
 
     private Position getPosition(ResultSet resultSet) throws SQLException {
-        return Position.valueOf(resultSet.getString(POSITION).toUpperCase());
+        String rawPosition = resultSet.getString(COLUMN_POSITION);
+
+        return Position.valueOf(rawPosition.toUpperCase());
     }
 }
